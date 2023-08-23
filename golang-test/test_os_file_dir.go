57ab9f27ec1a5180ecc1fa3e5c8c00df77ec2405
carlos <1447675994@qq.com>
2023-08-23 12:23:47 +0800

    test files

--- golang-test/test_os_file_dir.go
+++ golang-test/test_os_file_dir.go
@@ -1,11 +1,17 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"path"
+	"path/filepath"
+)
 
 func createFile() {
 	f, err := os.Create("a.txt")
 	if err != nil {
 		fmt.Printf("err:%v\n", err)
-	}ekse {
+	} else {
 		fmt.Printf("f.Name(): %v\n", f.Name())
 	}
 }
@@ -16,6 +22,17 @@ func makeDir() {
 		fmt.Printf("f dir err : %v\n", err)
 	}
 }
+
+func checkFileName() {
+	filename := "device/sdk/CMakeLists.acf"
+
+	filesuffix := path.Ext(filename)
+	pathDir := filepath.Dir(filename)
+	fmt.Println("file filesuffix:", filesuffix)
+
+	fmt.Println("file pathDir:", pathDir)
+}
+
 func main() {
-	createFile()
+	checkFileName()
 }
